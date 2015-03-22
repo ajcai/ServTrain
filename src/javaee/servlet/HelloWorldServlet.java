df769077b2ca89cb4136938291cada35277550c2
kc312546 <kc312546@foxmail.com>
2015-03-22 15:13:35 +0800

    first commit

---
+++ src/javaee/servlet/HelloWorldServlet.java
@@ -0,0 +1,36 @@
+package javaee.servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class HelloWorldServlet
+ */
+public class HelloWorldServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public HelloWorldServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.setContentType("text/html; charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("Hello World!");
+		out.println(request.getRemoteAddr());
+		out.println(request.getMethod());
+
+	}
+
+}
